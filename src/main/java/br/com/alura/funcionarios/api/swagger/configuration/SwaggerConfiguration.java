e1ef9c42f15d889ae7aea26d839f4f9c7c011726
thiagoandrade2019 <50715392+thiagoandrade2019@users.noreply.github.com>
2020-04-01 16:51:17 -0300

    first commit

---
+++ src/main/java/br/com/alura/funcionarios/api/swagger/configuration/SwaggerConfiguration.java
@@ -0,0 +1,36 @@
+package br.com.alura.funcionarios.api.swagger.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.models.Contact;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.alura.funcionarios.api"))
+				.build()
+				.apiInfo(getApiInfo());
+	}
+
+	private ApiInfo getApiInfo() {
+		
+		return new ApiInfoBuilder()
+				.title("Funcionários API ")
+				.description("Esta API faz o cadastro, consulta, alteração e exclusão de funcionários")
+				.contact(new springfox.documentation.service.Contact("Contato Thiago", "http://alura.com.br", "contato@alura.com.br"))
+				.version("1.0.0")
+				.build();
+	}
+}
